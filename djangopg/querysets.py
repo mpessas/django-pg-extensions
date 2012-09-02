1b0c0443159d3f27bf7ce6a784724d46a8c26b93
Apostolos Bessas <mpessas@transifex.com>
2012-09-02 13:07:19 +0300

    Allow to rank search results by relevancy
    The user may specify an ORDER BY clause that uses the ranking function
    of PostgreSQL for full text search.

--- djangopg/querysets.py
+++ djangopg/querysets.py
@@ -7,16 +7,32 @@ from django.utils.encoding import force_unicode
 class FtsQuerySet(models.query.QuerySet):
 
     def search_raw(self, **kwargs):
-        """Do a raw full-text search."""
+        """
+        Do a raw full-text search.
+
+        The user may also specify to order the results on how relevant they
+        are. For this, he has to specify a keyword argument,
+        ``order_by_field``. Using a minus in the field to control the
+        ordering also works.
+        """
         return self._search("to_tsquery", **kwargs)
 
     def search(self, **kwargs):
-        """Do a full-text search."""
+        """
+        Do a full-text search.
+
+        The user may also specify to order the results on how relevant they
+        are. For this, he has to specify a keyword argument,
+        ``order_by_field``. Using a minus in the field to control the
+        ordering also works.
+        """
         return self._search("plainto_tsquery", **kwargs)
 
-    def _search(self, func_name, **kwargs):
+    def _search(self, func_name, order_by_field=None, **kwargs):
         """Do a full-text search."""
         where = []
+        select = {}
+        order_by = []
         for search_field, query in kwargs.items():
             relation = None
             LOOKUP_SEP = models.sql.constants.LOOKUP_SEP
@@ -47,6 +63,23 @@ class FtsQuerySet(models.query.QuerySet):
             qn = connections[self.db].ops.quote_name
             field = '{table}.{column}'.format(
                 table=qn(db_table), column=qn(search_field))
-            where.append(unicode(" ({0}) @@ ({1})".format(field, ts_query)))
+            where.append(u" ({0}) @@ ({1})".format(field, ts_query))
 
-        return self.extra(where=where)
+            if order_by_field is not None:
+                if relation is None:
+                    order_key = search_field
+                else:
+                    order_key = relation + LOOKUP_SEP + search_field
+                order = order_by_field.get(order_key)
+                if order is None:
+                    continue
+                if order.startswith('-'):
+                    order = order[1:]
+                    sign = '-'
+                else:
+                    sign = ''
+                select[order] = u'ts_rank({0}, {1})'.format(field, ts_query)
+                order_by.append(
+                    '{sign}{field}'.format(sign=sign, field=order)
+                )
+        return self.extra(select=select, where=where, order_by=order_by)
