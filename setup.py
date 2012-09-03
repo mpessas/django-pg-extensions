5257fddd9f27e13b3f0fa0f68832e23068ea2d5f
Apostolos Bessas <mpessas@transifex.com>
2012-09-03 16:06:56 +0300

    Add setup.py file.

---
+++ setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name = "django-pg-extensions",
+    version = "0.1devel",
+    description = "Extensions for Django to fully utilize PostgreSQL.",
+    author = "Apostolos Bessas",
+    author_email = "mpessas@gmail.com",
+    packages=["djangopg"]
+)
