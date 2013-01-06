ca822aa03ea9f5752832d02c973208d75759021d
Apostolos Bessas <mpessas@transifex.com>
2013-01-06 21:55:13 +0200

    Update dependencies

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name = "django-pg-extensions",
@@ -8,5 +8,7 @@ setup(
     description = "Extensions for Django to fully utilize PostgreSQL.",
     author = "Apostolos Bessas",
     author_email = "mpessas@gmail.com",
-    packages=["djangopg"]
+    packages=["djangopg"],
+    install_requires=["Django", ],
+    tests_require=["django-discover-runner", "mock", ]
 )
