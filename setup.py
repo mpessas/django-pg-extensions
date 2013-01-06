f76a532890d911e055f0508aebe815ff3b3e1425
Apostolos Bessas <mpessas@transifex.com>
2013-01-06 21:58:17 +0200

    Style fixes for setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from setuptools import setup
 
 setup(
-    name = "django-pg-extensions",
-    version = "0.1devel",
-    description = "Extensions for Django to fully utilize PostgreSQL.",
-    author = "Apostolos Bessas",
-    author_email = "mpessas@gmail.com",
+    name="django-pg-extensions",
+    version="0.1devel",
+    description="Extensions for Django to fully utilize PostgreSQL.",
+    author="Apostolos Bessas",
+    author_email="mpessas@gmail.com",
     packages=["djangopg"],
     install_requires=["Django", ],
     tests_require=["django-discover-runner", "mock", ]
