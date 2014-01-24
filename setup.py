e9bca9b7d66d5f323f5d5882ea9db7f67753174e
Apostolos Bessas <mpessas@gmail.com>
2014-01-24 12:45:45 +0200

    Bump version to 0.1.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="django-pg-extensions",
-    version="0.1.4",
+    version="0.1.5",
     description="Extensions for Django to fully utilize PostgreSQL.",
     author="Apostolos Bessas",
     author_email="mpessas@gmail.com",
