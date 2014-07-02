8b164a20eaf095c3bf011e55c426ec9dce03f5e2
Apostolos Bessas <mpessas@gmail.com>
2014-07-02 15:04:47 +0300

    Drop casting to text for LIKE queries
    You should never use non-strings for LIKE queries. The change also
    restores case-insensitive behavior for citext.

--- djangopg/postgresql_psycopg2/base.py
+++ djangopg/postgresql_psycopg2/base.py
@@ -1,6 +1,19 @@
 # -*- coding: utf-8 -*-
 
 from django.db.backends.postgresql_psycopg2.base import DatabaseWrapper
+from django.db.backends.postgresql_psycopg2.operations import (
+    DatabaseOperations as BaseOperations
+)
+
+
+class DatabaseOperations(BaseOperations):
+
+    def lookup_cast(self, lookup_type):
+        lookup = '%s'
+        # Use UPPER(x) for case-insensitive lookups; it's faster.
+        if lookup_type in ('iexact', 'icontains', 'istartswith', 'iendswith'):
+            lookup = 'UPPER(%s)' % lookup
+        return lookup
 
 
 class DatabaseWrapper(DatabaseWrapper):
@@ -8,6 +21,7 @@ class DatabaseWrapper(DatabaseWrapper):
 
     def __init__(self, *args, **kwargs):
         super(DatabaseWrapper, self).__init__(*args, **kwargs)
+        self.ops = DatabaseOperations(self)
         self.operators.update(
             {
                 'array_contains': '@> %s',
